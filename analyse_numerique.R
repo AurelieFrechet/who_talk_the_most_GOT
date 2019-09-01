6b13d9c61b34ae059e114d2bbf1e69f7359b18fc
Aurélie Fréchet <aurelie.frechet@gmail.com>
2019-09-01 08:15:57 +0200

    corrections fonctions

--- analyse_numerique.R
+++ analyse_numerique.R
@@ -2,6 +2,8 @@
 
 
 
+
+
 # 10 - Chargement des données et des packages -----------------------------
 
 library(data.table)
@@ -37,17 +39,23 @@ stats_GOT <-
 stats_GOT_nb_personnages <-
   dcast(stats_GOT, N_serie ~ allegiance, value.var = "nb_personnages") %>%
   as.data.table()
-stats_GOT_nb_personnages <- stats_GOT_nb_personnages[ , lapply(.SD, function(x) ifelse(is.na(x), 0, x))]
+stats_GOT_nb_personnages <-
+  stats_GOT_nb_personnages[, lapply(.SD, function(x)
+    ifelse(is.na(x), 0, x))]
 
 stats_GOT_nb_lignes <-
-  dcast(stats_GOT, N_serie ~ allegiance, value.var = "nb_lignes_dialogue")%>%
+  dcast(stats_GOT, N_serie ~ allegiance, value.var = "nb_lignes_dialogue") %>%
   as.data.table()
-stats_GOT_nb_lignes <- stats_GOT_nb_lignes[ , lapply(.SD, function(x) ifelse(is.na(x), 0, x))]
+stats_GOT_nb_lignes <-
+  stats_GOT_nb_lignes[, lapply(.SD, function(x)
+    ifelse(is.na(x), 0, x))]
 
 stats_GOT_nb_mots <-
-  dcast(stats_GOT, N_serie ~ allegiance, value.var = "nb_mots")%>%
+  dcast(stats_GOT, N_serie ~ allegiance, value.var = "nb_mots") %>%
   as.data.table()
-stats_GOT_nb_mots <- stats_GOT_nb_mots[ , lapply(.SD, function(x) ifelse(is.na(x), 0, x))]
+stats_GOT_nb_mots <-
+  stats_GOT_nb_mots[, lapply(.SD, function(x)
+    ifelse(is.na(x), 0, x))]
 
 
 
@@ -56,20 +64,26 @@ stats_GOT_nb_mots <- stats_GOT_nb_mots[ , lapply(.SD, function(x) ifelse(is.na(x
 ## Paramètres d'entrée :
 # - dimension 1 : numéro d'épisode ou saison
 # - dimension 2 : personnage ou alliégeance
-## Sortie : 
+## Sortie :
 # - Tableau croisé pour dessin graphique
 # - Tableau pour enrichir les stats en textuel
 
-tableaucroise_2D_GOT <- function(data, dimension1, dimension2, statistique){
-  result <-
-    dcast(data, dimension1 ~ dimension2, value.var = "nb_mots")%>%
-    as.data.table()
-  result <- result[ , lapply(.SD, function(x) ifelse(is.na(x), 0, x))]
-  return((result))
-}
+tableaucroise_2D_GOT <-
+  function(data, dimension1, dimension2, statistique) {
+    result <-
+      dcast(data, eval(parse(text = paste(dimension1, "~" ,dimension2))), value.var = "nb_mots") %>%
+      as.data.table()
+    result <-
+      result[, lapply(.SD, function(x)
+        ifelse(is.na(x), 0, x))]
+    return((result))
+  }
+# test <- tableaucroise_2D_GOT(data = stats_GOT,
+#                              dimension1 = "N_serie", 
+#                              dimension2 = "allegiance", 
+#                              statistique = "nb_mots")
 
-stat_2D_GOT <- function(dimension1, dimension2){
-  
+stat_2D_GOT <- function(dimension1, dimension2) {
   # Table dim1*dim2*stats
   stats_GOT <-
     data_GOT[order(Season, N_Season), .(
@@ -77,7 +91,7 @@ stat_2D_GOT <- function(dimension1, dimension2){
       nb_lignes_dialogue = .N,
       nb_mots = sum(nbwords)
     ),
-    by = .(dimension1,
+    by = c(dimension1,
            dimension2)]
   
   stats_GOT_nb_personnages <-
@@ -88,7 +102,7 @@ stat_2D_GOT <- function(dimension1, dimension2){
       statistique = "nb_personnages"
     )
   
- 
+  
   stats_GOT_nb_lignes <-
     tableaucroise_2D_GOT(
       data = stats_GOT,
@@ -97,15 +111,19 @@ stat_2D_GOT <- function(dimension1, dimension2){
       statistique = "nb_lignes_dialogue"
     )
   
-   stats_GOT_nb_mots <-
-     tableaucroise_2D_GOT(
-       data = stats_GOT,
-       dimension1 = dimension1,
-       dimension2 = dimension2,
-       statistique = "nb_mots"
-     )
+  stats_GOT_nb_mots <-
+    tableaucroise_2D_GOT(
+      data = stats_GOT,
+      dimension1 = dimension1,
+      dimension2 = dimension2,
+      statistique = "nb_mots"
+    )
   
-  
+  return(list(stats_GOT, 
+              stats_GOT_nb_personnages, 
+              stats_GOT_nb_lignes, 
+              stats_GOT_nb_mots))
 }
 
-
+test <- stat_2D_GOT(dimension1 = "N_serie", 
+                    dimension2 = "allegiance")
